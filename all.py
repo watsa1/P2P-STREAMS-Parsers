cebec787e920c69e8189aedb141563ce2873bd07
enen92 <enen92@users.noreply.github.com>
2014-07-29 21:25:36 +0100

    Update all.py

--- all.py
+++ all.py
@@ -5,6 +5,6 @@ all_modules = [ 'http://github.com/enen92/P2P-STREAMS-Parsers/blob/master/1Torre
 
 for parser in all_modules:
 	xbmc.executebuiltin('XBMC.RunPlugin("plugin://plugin.video.p2p-streams/?mode=405&name=p2p&url=' + urllib.quote(parser) + '")')
-	xbmc.sleep(5000)
+	xbmc.sleep(10000)
 
 xbmc.executebuiltin("Notification(%s,%s,%i,%s)" % ('P2P-Streams', "All parsers imported",1,''))
